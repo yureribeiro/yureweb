3be298f802cdbca36faf26272b35a5990d48c551
Mac <mac@MacBook-Pro-de-Mac.local>
2023-05-29 06:49:53 -0300

    adicionando mockup bittcsy na pagina projetos

--- src/contents/projects/index.jsx
+++ src/contents/projects/index.jsx
@@ -1,8 +1,10 @@
 import React, {useState}from "react"
 import CardProject from "../../components/cardProjeto"
-import QRcode from "../../assets/QRcode.png"
 import styles from "./projects.module.css"
+
 import arrowRight from "../../assets/arrow-right.svg"
+import QRcode from "../../assets/QRcode.png"
+import BittcsyMockup from '../../assets/bittcsy.png'
 
 function Projects () {
 
@@ -12,20 +14,12 @@ function Projects () {
       tech: "React Native - Expo - Node - Prisma - Postgres",
       description:
         "O Bittcsy é um aplicativo móvel, que permite aos usuários acompanhar o mercado e verificar o status das criptomoedas, ler as últimas notícias relacionadas ao mundo cripto, moedas mais pesquisadas e participar do forum de discussões.",
-      image: QRcode,
+      image: BittcsyMockup,
       link: "https://play.google.com/store/apps/details?id=com.yureribeirodev.Bittcsy",
       titleLink: "Disponível na Google Play",
     },
     {
       title: "QRcodee",
-      description:
-        "Gerador de QRcode, basta inserir o texto ou link no input, será gerado um QR para download em PNG para compartilhar com quem quiser.",
-      image: QRcode,
-      link: "https://qrcodee.vercel.app",
-      titleLink: "Acesse QRcodee aqui",
-    },
-    {
-      title: "Gerador de QRcode",
       description:
         "Gerador de QRcode, basta inserir o texto ou link no input, será gerado um QR para download em PNG para compartilhar com quem quiser.",
       image: QRcode,
